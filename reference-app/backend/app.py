46ea6b9f06b81cb9028e9fc653bf54e1429fd6b5
Ammar Yasser <aerosound161@gmail.com>
2022-09-27 17:50:46 +0200

    different way for doing tracing

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask ,Response , render_template, request, jsonify
 from prometheus_flask_exporter import PrometheusMetrics
 from jaeger_client import Config
 import logging
+from flask_opentracing import FlaskTracing
 
 
 import pymongo
@@ -9,6 +10,7 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+JAEGER_DOMAIN_NAME = 'traces-collection-agent.observability.svc.cluster.local'
 
 metrics.info('app_info', 'Application info', version='1.0.3')
 
@@ -23,7 +25,8 @@ def init_tracer(service='backend'):
     )
     return config.initialize_tracer()
 
-jaeger_tracer = init_tracer()
+jaeger_tracer = init_tracer('backend')
+tracing = FlaskTracing(jaeger_tracer, True, app)
 
 app.config["MONGO_DBNAME"] = "example-mongodb"
 app.config[
@@ -35,8 +38,6 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def homepage():
-    with jaeger_tracer.start_span('home-span') as span:
-        span.set_tag('homepage', 15)
     if request.headers['error'] == 'client':
         return Response(status=404)
     if request.headers['error'] == 'server':
@@ -46,16 +47,12 @@ def homepage():
 
 @app.route("/api")
 def my_api():
-    with jaeger_tracer.start_span('api-span') as span:
-        span.set_tag('api', 16)
     answer = "something"
     return jsonify(repsonse=answer)
 
 
 @app.route("/star", methods=["POST"])
 def add_star():
-    with jaeger_tracer.start_span('star-span') as span:
-        span.set_tag('star', 17)
     star = mongo.db.stars
     name = request.json["name"]
     distance = request.json["distance"]
